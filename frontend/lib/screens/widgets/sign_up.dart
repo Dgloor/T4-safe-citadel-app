3a386ee210792eebe03674d921fade5353b0e72a
jjgilces <jjgilces@espol.edu.ec>
2023-06-17 21:36:28 -0500

    edit model

--- frontend/lib/screens/widgets/sign_up.dart
+++ frontend/lib/screens/widgets/sign_up.dart
@@ -24,206 +24,61 @@ class SignUpState extends State<SignUpWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      flex:1,
-      // padding: EdgeInsets.only(top: 30.0),
+    return   Container(
+      width: 5,
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      child: Form(
         child:Column(
-        children: <Widget>[
-          Stack(
-            alignment: Alignment.topCenter,
-            clipBehavior: Clip.none,
-            children: <Widget>[
-              Container(
-                color: Colors.white,
-                // shape: RoundedRectangleBorder(
-                //     borderRadius: BorderRadius.circular(8.0)),
-                child: Container(
-                  height: 480.00,
-                  child: Column(
-                    children: <Widget>[ 
-                      Padding(
-                        padding: EdgeInsets.only(
-                            top: 20.0, bottom: 20.0, left: 25.0, right: 25.0),
-                        child: TextField(
-                          focusNode: focusName,
-                          controller: nameController,
-                          keyboardType: TextInputType.emailAddress,
-                          style: TextStyle(
-                              fontFamily: "SignikaSemiBold",
-                              fontSize: 16.0,
-                              color: Colors.black),
-                          decoration: InputDecoration(
-                              border: InputBorder.none,
-                              icon: Icon(
-                                FontAwesomeIcons.envelope,
-                                color: Colors.black,
-                                size: 22.0,
-                              ),
-                              hintText: "Enter name",
-                              hintStyle: TextStyle(
-                                  fontFamily: "SignikaSemiBold",
-                                  fontSize: 18.0)),
-                        ),
-                      ),
-                      Container(
-                        width: 250.0,
-                        height: 1.0,
-                        color: Colors.grey,
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(
-                            top: 20.0, bottom: 20.0, left: 25.0, right: 25.0),
-                        child: TextField(
-                          focusNode: focusEmail,
-                          controller: emailController,
-                          keyboardType: TextInputType.emailAddress,
-                          style: TextStyle(
-                              fontFamily: "SignikaSemiBold",
-                              fontSize: 16.0,
-                              color: Colors.black),
-                          decoration: InputDecoration(
-                              border: InputBorder.none,
-                              icon: Icon(
-                                FontAwesomeIcons.envelope,
-                                color: Colors.black,
-                                size: 22.0,
-                              ),
-                              hintText: "Enter email",
-                              hintStyle: TextStyle(
-                                  fontFamily: "SignikaSemiBold",
-                                  fontSize: 18.0)),
-                        ),
-                      ),
-                      Container(
-                        width: 250.0,
-                        height: 1.0,
-                        color: Colors.grey,
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(
-                            top: 20.0, bottom: 20.0, left: 25.0, right: 25.0),
-                        child: TextField(
-                          focusNode: focusPassword,
-                          controller: passwordController,
-                          style: TextStyle(
-                              fontFamily: "SignikaSemiBold",
-                              fontSize: 16.0,
-                              color: Colors.black),
-                          decoration: InputDecoration(
-                              border: InputBorder.none,
-                              icon: Icon(
-                                FontAwesomeIcons.lock,
-                                color: Colors.black,
-                                size: 22.0,
-                              ),
-                              hintText: "Enter password",
-                              hintStyle: TextStyle(
-                                  fontFamily: "SignikaSemiBold",
-                                  fontSize: 18.0)),
-                        ),
-                      ),
-                      Container(
-                        width: 250.0,
-                        height: 1.0,
-                        color: Colors.grey,
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(
-                            top: 20.0, bottom: 20.0, left: 25.0, right: 25.0),
-                        child: TextField(
-                          focusNode: focusPassword,
-                          controller: passwordController,
-                          style: TextStyle(
-                              fontFamily: "SignikaSemiBold",
-                              fontSize: 16.0,
-                              color: Colors.black),
-                          decoration: InputDecoration(
-                              border: InputBorder.none,
-                              icon: Icon(
-                                FontAwesomeIcons.lock,
-                                color: Colors.black,
-                                size: 22.0,
-                              ),
-                              hintText: "Confirm password",
-                              hintStyle: TextStyle(
-                                  fontFamily: "SignikaSemiBold",
-                                  fontSize: 18.0)),
-                        ),
-                      ),
-                      Container(
-                        width: 250.0,
-                        height: 1.0,
-                        color: Colors.grey,
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(top: 40.0),
-                        decoration: BoxDecoration(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(5.0)),
-                            boxShadow: <BoxShadow>[
-                              BoxShadow(
-                                  color: AppColours.colorStart,
-                                  offset: Offset(1.0, 6.0),
-                                  blurRadius: 20.0),
-                              BoxShadow(
-                                  color: AppColours.colorEnd,
-                                  offset: Offset(1.0, 6.0),
-                                  blurRadius: 20.0),
-                            ],
-                            gradient: LinearGradient(
-                                colors: [
-                                  AppColours.colorEnd,
-                                  AppColours.colorStart
-                                ],
-                                begin: const FractionalOffset(0.2, 0.2),
-                                end: const FractionalOffset(1.0, 1.0),
-                                stops: [0.1, 1.0],
-                                tileMode: TileMode.clamp)),
-                        child: MaterialButton(
-                          highlightColor: Colors.transparent,
-                          splashColor: AppColours.colorEnd,
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                                vertical: 10.0, horizontal: 42.0),
-                            child: Text(
-                              "Sign Up",
-                              style: TextStyle(
-                                  fontFamily: "SignikaSemiBold",
-                                  color: Colors.white,
-                                  fontSize: 22.0),
-                            ),
-                          ),
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                        ),
-                      )
-                    ],
-                  ),
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextFormField(
+              decoration: const  InputDecoration(
+                label: Text('Name'),
+                prefixIcon: Icon(
+                  Icons.person,
+                  color:Colors.green,
                 ),
-              ),
-            ],
-          ),
-          Container(
-            decoration: BoxDecoration(
-              gradient: new LinearGradient(
-                  colors: [
-                    Colors.white10,
-                    Colors.white,
-                  ],
-                  begin: const FractionalOffset(0.0, 0.0),
-                  end: const FractionalOffset(1.0, 1.0),
-                  stops: [0.0, 1.0],
-                  tileMode: TileMode.clamp),
+                border: OutlineInputBorder(),
+                labelStyle: TextStyle(color: Colors.grey),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black, width: 1.0),
+                ),
+              )
             ),
-            width: 100.0,
-            height: 1.0,
-          ),
-        
-         
-    
-        ],
-      ),
-     ) ;
+              TextFormField(
+              decoration: const  InputDecoration(
+                label: Text('Email'),
+                prefixIcon: Icon(
+                  Icons.mail,
+                  color:Colors.green,
+                ),
+                border: OutlineInputBorder(),
+                labelStyle: TextStyle(color: Colors.grey),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black, width: 1.0),
+                ),
+              )
+            ),
+              TextFormField(
+              decoration: const  InputDecoration(
+                label: Text('Phone'),
+                prefixIcon: Icon(
+                  Icons.phone,
+                  color:Colors.green,
+                ),
+                border: OutlineInputBorder(),
+                labelStyle: TextStyle(color: Colors.grey),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black, width: 1.0),
+                ),
+              )
+            )
+          ],
+        )
+      )
+    );
+     
+
+       
   }
 }
