a0414d4c4cb35a61180ccbac1852ebd575676ede
alanbarco <alaxbarc@espol.edu.ec>
2023-06-27 15:15:30 -0500

    update

--- frontend/test/register_test.dart
+++ frontend/test/register_test.dart
@@ -13,7 +13,6 @@ void main() {
     await tester.tap(registerVisitButton);
     await tester.pump();
 
-    
     expect(find.text("Ingrese nombre de la visita"), findsOneWidget);
   });
   testWidgets("Visitor name is not empty", (WidgetTester tester) async{
@@ -24,24 +23,40 @@ void main() {
     await tester.pumpWidget(MaterialApp(home: Body()));
     await tester.enterText(visitorNameField, "Juan Perez");
     await tester.tap(registerVisitButton);
-    await tester.pump();
-
-    
+    await tester.pump(Duration(seconds: 10));   
     expect(find.text("Enviar código QR al visitante"), findsOneWidget);
   });
 
 
-//  testWidgets("Validation for visitor name field", (WidgetTester tester) async{
-//     final testTime = TimeOfDay(hour: 17, minute: 30);
-//     final selectedTime = find.byKey(ValueKey("selectedTime"));
-//     final selectedDay = find.byKey(ValueKey("selectedDay"));
-//     final registerVisitButton = find.byKey(ValueKey("registerVisitButton"));
+// testWidgets('CupertinoDatePicker selected time test', (WidgetTester tester) async {
+//     DateTime selectedDateTime;
 
+//     await tester.pumpWidget(MaterialApp(home: Body())
+//     );
 
-//     await tester.tap(selectedTime);
-//     await tester.tap(registerVisitButton);
-//     await tester.pump();
-//     expect(find.text("Hora de visita no válida"), findsOneWidget);
-//    });
+//     // Toca el widget para abrir el selector de fecha y hora
+//     await tester.tap(find.byType(CupertinoDatePicker));
+
+//     // Espera a que se muestre el selector de fecha y hora
+//     await tester.pumpAndSettle();
+
+//     // Selecciona la hora 13:45
+//     await tester.dragUntilVisible(
+//       find.text('1'), // Encuentra el número 1 en el selector de horas
+//       find.byType(CupertinoPicker),
+//       const Offset(0, -300),
+//     );
+
+//     await tester.dragUntilVisible(
+//       find.text('45'), // Encuentra el número 45 en el selector de minutos
+//       find.byType(CupertinoPicker),
+//       const Offset(0, -100),
+//     );
+
+//     // Asegura que se haya seleccionado la hora 13:45
+//     expect(selectedDateTime.hour, 13);
+//     expect(selectedDateTime.minute, 45);
+//   });
+// }
     
 }
