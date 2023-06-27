42cbd1f3d9f2729c93f986b6fc33a618edb7c7ea
alanbarco <alaxbarc@espol.edu.ec>
2023-06-27 16:49:21 -0500

    test cases for visit registration

--- frontend/test/register_test.dart
+++ frontend/test/register_test.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:prueba/screens/register/components/body.dart';
 
+
 void main() {
   testWidgets("Visitor name field is empty", (WidgetTester tester) async{
     final visitorNameField = find.byKey(ValueKey("visitorNameField"));
@@ -15,7 +16,9 @@ void main() {
 
     expect(find.text("Ingrese nombre de la visita"), findsOneWidget);
   });
-  testWidgets("Visitor name is not empty", (WidgetTester tester) async{
+
+
+  testWidgets("Visitor name field is not empty", (WidgetTester tester) async{
     final visitorNameField = find.byKey(ValueKey("visitorNameField"));
     final registerVisitButton = find.byKey(ValueKey("registerVisitButton"));
 
@@ -24,39 +27,7 @@ void main() {
     await tester.enterText(visitorNameField, "Juan Perez");
     await tester.tap(registerVisitButton);
     await tester.pump(Duration(seconds: 10));   
-    expect(find.text("Enviar código QR al visitante"), findsOneWidget);
+    expect(find.byKey(ValueKey("qrCode")), findsOneWidget);
   });
 
-
-// testWidgets('CupertinoDatePicker selected time test', (WidgetTester tester) async {
-//     DateTime selectedDateTime;
-
-//     await tester.pumpWidget(MaterialApp(home: Body())
-//     );
-
-//     // Toca el widget para abrir el selector de fecha y hora
-//     await tester.tap(find.byType(CupertinoDatePicker));
-
-//     // Espera a que se muestre el selector de fecha y hora
-//     await tester.pumpAndSettle();
-
-//     // Selecciona la hora 13:45
-//     await tester.dragUntilVisible(
-//       find.text('1'), // Encuentra el número 1 en el selector de horas
-//       find.byType(CupertinoPicker),
-//       const Offset(0, -300),
-//     );
-
-//     await tester.dragUntilVisible(
-//       find.text('45'), // Encuentra el número 45 en el selector de minutos
-//       find.byType(CupertinoPicker),
-//       const Offset(0, -100),
-//     );
-
-//     // Asegura que se haya seleccionado la hora 13:45
-//     expect(selectedDateTime.hour, 13);
-//     expect(selectedDateTime.minute, 45);
-//   });
-// }
-    
 }
